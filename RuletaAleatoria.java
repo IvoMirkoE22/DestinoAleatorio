79d32406e01e7e9c33fe0dae01fe130b12eb23bb
IvoMirkoE22 <ivomirkoelian20@gmail.com>
2025-07-04 12:53:43 -0300

    Agregada clase RuletaAleatoria con métodos funcionales y documentación Javadoc

---
+++ RuletaAleatoria.java
@@ -0,0 +1,79 @@
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Random;
+/**
+ * La clase RuletaAleatoria permite agregar nombres únicos a una lista y seleccionar
+ * uno de ellos de forma aleatoria, simulando el comportamiento de una ruleta.
+ * Tambíen proporciona métodos para mostrar la lista de nombres actuales
+ * 
+ * Evita la repetición de nombres y lanza excepciones en caso de operaciones inválidas,
+ * como intentar girar la ruleta sin nombres cargados.
+ * 
+ * @author (Ivo Narváez) 
+ * @version (1.0)
+ */
+public class RuletaAleatoria
+{
+    private ArrayList<String> nombres;
+    private HashSet<String> nombresUnicos;
+    private Random random;
+    
+    /**
+     * Constructor de la clase RuletaAleatoria
+     * Inicializa la lista de nombres, el conjunto de nombres únicos (para evitar repetidos),
+     * y un generador de números aleatorios.
+     */
+    public RuletaAleatoria()
+    {
+        nombres = new ArrayList<>();
+        nombresUnicos = new HashSet<>();
+        random = new Random();
+    }
+    
+    /**
+     * Agrega un nuevo nombre a la ruleta si aún no ha sido ingresado.
+     * La verificación es insensible a mayúsculas/minúsculas.
+     * 
+     * @param  nombre  que ingresa el usuario para este ser agregado a la lista
+     * @return true si el nombre fue agregado exitosamente.
+     * @throws IllegalArgumentException si el nombre ya existe en la lista.
+     */
+    public boolean agregarNombres(String nombre){
+        if(nombresUnicos.contains(nombre.toLowerCase())){
+            throw new IllegalArgumentException("Este nombre ya fue añadido con anterioridad");
+        }
+        nombres.add(nombre);
+        nombresUnicos.add(nombre.toLowerCase());
+        return true;
+    }
+    
+    /**
+     * Gira la ruleta seleccionando aleatoriamente uno de los nombres ingresados.
+     * @throws IllegalStateException si no hay nombre en la ruleta.
+     */
+    public void girarRuleta(){
+        if(nombres.isEmpty()){
+            throw new IllegalStateException("La lista esta vacia, ingresa almenos un nombre");
+        }
+        int index = random.nextInt(nombres.size());
+        String nombre = nombres.get(index);
+        
+        System.out.println("El nombre elegido es: "+ nombre);
+        
+    }
+    /**
+     * Muestra todos los nombres actualmente agregados a la ruleta
+     * @throws IllgalStateEception si la lista de nombres está vacía.
+     */
+    public void mostrarNombres(){
+        if(nombres.isEmpty()){
+            throw new IllegalStateException("La lista es vacia");
+        }
+        System.out.println("Nombres de la ruleta");
+        for(String nombre : nombres){
+            System.out.println(" - " + nombre);
+        }
+    }
+    
+    
+}
